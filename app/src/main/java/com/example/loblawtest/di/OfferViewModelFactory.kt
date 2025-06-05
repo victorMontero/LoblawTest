aa9497e9a68f9e9ee8180399794941bd70da75e4
victorMontero <viiktoor@gmail.com>
2025-06-05 16:56:41 -0400

    fixing factory bug

--- app/src/main/java/com/example/loblawtest/di/OfferViewModelFactory.kt
+++ app/src/main/java/com/example/loblawtest/di/OfferViewModelFactory.kt
@@ -18,7 +18,7 @@ class OfferViewModelFactory : ViewModelProvider.Factory {
     }
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(OfferViewModelFactory::class.java)) {
+        if (modelClass.isAssignableFrom(OfferViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return OfferViewModel(offerRepository) as T
         }
